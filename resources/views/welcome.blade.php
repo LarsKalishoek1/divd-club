38ece120390524740698d4bec21103c38cc5610b
LarsKalishoek1 <lars.kalishoek@student.divd.nl>
2023-01-16 13:48:25 +0100

    push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>DIVD.club</title>
+  <meta name="description" content="DIVD.club">
+  <meta name="author" content="DIVD.club">
+
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+  <meta property="og:title" content="DIVD.club">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://DIVD.club">
+  <meta property="og:description" content="DIVD.club">
+
+  <link rel="stylesheet" href="assetsstyle.css">
+
+</head>
+<body>
+  <br /><br /><br /><br /><br />
+  <div class="img-container-block">
+    <img src="DIVD.club.logo.png"  alt="DIVD.club">
+  </div>
+  <br><br>
+  <div class="text-block">
+    <h2>Welkom!</h2>
+    <p>Goed dat je er bent! De DIVD club is een plek waar kinderen en jongeren 
+	samen kunnen leren over computerbeveiliging, hacking, 
+        het verbeteren van de (internet-)wereld en véél meer!<br></p>
+
+  <div class="list-block">
+  <p>Ben jij geïnteresseerd in…</p>
+        <ul>
+        <li> Hoe cybercriminelen steeds weer in weten te breken?</li>
+        <li> Hoe je jezelf tegen deze mensen moet verdedigen?</li>
+        <li> Hoe jij kan bijdragen aan een veiligere wereld?</li>
+        </ul>
+  </div>
+
+  </div>
+
+
+  <div class="text-block">      
+  <p>...of ben je gewoon geïnteresseerd in cyber security en wil je een keer een kijkje komen nemen? 
+	  Kom gerust een keer met ons praten in onze <a href="https://discord.gg/yCsXtFMVVh" target="_blank">Discord</a>. Wij leren je met alle plezier 
+  hoe jij jezelf digitaal kan verdedigen en hoe jij zelf kan beginnen met bijdragen aan de reis naar 
+  een veiliger internet! Klik hieronder op de inschrijfknop en hopelijk zien we je daar! <br><br>Let op; als 
+  je jonger bent dan 18 moet je eerst toestemming vragen aan je ouders!</p><br>
+
+    <a href="https://docs.google.com/forms/d/10r-a-3qXBBZASK8QTo0Et9dgRSiEOttf8L1wWun_LR4" class="button2">Kom erbij!</a>
+  
+  </div><br>
+  
+  
+  <div class="text-block">
+  <h2>Voor ouders</h2>
+  <p>Het <a href="https://www.divd.nl">DIVD (Dutch Institute for Vulnerability Disclosure)</a> is een groep vrijwilligers die zich inzet 
+  voor een veiliger internet. Naast het doen van <a href="https://www.onderzoeksraad.nl/nl/media/attachment/2021/12/16/kwetsbaar_door_software_lessen_naar_aanleiding_van_beveiligingslekken_door_sof-286451296.pdf" target="_blank">beveiligingsonderzoek</a> vinden wij het belangrijk dat er 
+  tijd wordt besteed aan zowel het opleiden en begeleiden van de volgende generatie hackers als het 
+  opvangen van de risicogroep jongeren die een veilige plek nodig hebben om te leren. <br><br>
+  
+  Bij de DIVD club kunnen jongeren terecht voor leerpaden, vragen en begeleiding in het thema 
+  van computerbeveiliging. Dit wordt geleid door ervaren professionals in de cyber security branche 
+  welke beschikbaar zijn voor de jongeren als zij uitleg of sturing nodig hebben. <br><br>
+  
+  Het DIVD werkt nauw samen met organisaties als het <a href="https://www.ncsc.nl">Nationaal Cyber Security Centrum (NCSC)</a>, 
+  het <a href="https://www.digitaltrustcenter.nl">Digital Trust Center (DTC)</a> en initiatieven als <a href="https://www.reclassering.nl/actueel/verhalen/hack_right-gericht-op-recidivevermindering-bij-jonge-hackers">Hack_Right</a>. <br><br>
+  
+  Voor vragen over de DIVD club kunt u ons bereiken via <a href="mailto:info@divd.club">info@divd.club</a>.
+  </p>
+  
+ <p>Zie je iets wat technisch niet in orde of niet <a href="https://internet.nl/site/divd.club/1502532/">veilig</a> genoeg is op deze website dan kun je dat <a href="https://app.zerocopter.com/nl/rd/f9afbf32-ce59-48ed-96f2-ac4410595aa4">hier</a> melden.</p>
+
+</body>
+</html>
+
